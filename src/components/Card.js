f75d2043b5a73da5775e966cac6c212299a92a76
Francis Bello <bellofrancis87@gmail.com>
2022-01-06 11:31:48 +0100

    'fixed image not showing'

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,11 @@ import star from "../images/Star 1.png";
 export default function Card(props) {
   return (
     <div className="card-content">
-      <img src={props.coverImg} className="card-pic" alt="user pic" />
+      <img
+        src={process.env.PUBLIC_URL + props.img}
+        className="card-pic"
+        alt="user pic"
+      />
       <div className="rating">
         <img src={star} alt="rating star" className="rating-" />
         <span className="rating-num">{props.rating}</span>
@@ -17,7 +21,9 @@ export default function Card(props) {
       <p>
         <span>From ${props.price}</span> / person
       </p>
-      <p>OpenSpots: {props.openSpots}</p>
+      <p>
+        <strong>OpenSpots:</strong> {props.openSpots}
+      </p>
     </div>
   );
 }
